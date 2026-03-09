92f54465308a12bdc1f341f20f2ba65c099423a5
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 18:28:02 +0000

    move to tests/ folder

---
+++ src/com/numvex/tests/Test.java
@@ -0,0 +1,11 @@
+package com.numvex.tests;
+
+import com.numvex.Array;
+import com.numvex.typing.Types;
+
+public class Test {
+    public static void main(String[] args) {
+        // test the Array(type,...elements)
+        Array typed_array = new Array(Types.FLOAT, 0.1, 1.2, 55.2, 3.2);
+    }
+}
