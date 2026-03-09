02ec4d823f7dba84927f9eacb9c8b13233e36dbe
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 16:44:07 +0000

    make test for typed Array(type,..elements)

--- src/com/numvex/Test.java
+++ src/com/numvex/Test.java
@@ -1,7 +1,11 @@
 package com.numvex;
 
+import com.numvex.Array;
+import com.numvex.typing.Types;
 
-// to see if everything works well
 public class Test {
-	
+    public static void main(String[] args) {
+        // test the Array(type,...elements)
+        Array typed_array = new Array(Types.FLOAT, 0.1, 1.2, 55.2, 3.2);
+    }
 }
