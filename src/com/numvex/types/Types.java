7f2ea36faa9b4b924a9ad0f45cff5cbe52c3074d
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 15:40:18 +0000

    .

--- src/com/numvex/types/Types.java
+++
@@ -1,9 +0,0 @@
-
-public final class Types {
-    // define types strings
-    public final String DOUBLE = "double",
-            STRING = "string",
-            INT = "integer",
-            BOOLEAN = "boolean";
-
-}
