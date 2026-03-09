e40cc93d51699bbd9b877efd397c1ee44fd440ae
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 15:35:23 +0000

    Array Tools

---
+++ src/com/numvex/utils/Types.java
@@ -0,0 +1,15 @@
+
+public final class Types {
+    // define types strings
+    public final String 
+        DOUBLE = "double",
+        STRING = "string",
+        INT = "integer",
+        BOOLEAN = "boolean";
+    public static final String SUPPORTED_TYPES[] = {
+        Types.DOUBLE,
+        Types.INT,
+        Types.STRING,
+        Types.BOOLEAN
+    };
+}
---
+++ src/com/numvex/utils/arrays/Array.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * This class provides u to create an array of a given type:str
+ * - structure of the constructor : 
+ *      Array(type:Types.* , **elements)
+ * - example : 
+ *  Array test = new Array(Types.STRING , "ll","bb",...)
+ */
+
+package com.numvex; 
+import com.numvex.utils.Types;
+
+
+public class Array<T> {
+
+    private final Object[] data;
+    private final String type;
+    private final size;
+
+    private final String[] allowed_types = Types.SUPPORTED_TYPES;
+
+    public Array()
+    }
