d096c3a87a6d34f51da33beebad31fd0b3e682f9
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 18:45:25 +0000

    move Test.java to tests/ directory

---
+++ src/com/numvex/utils/tests/Test.java
@@ -0,0 +1,15 @@
+package com.numvex.tests;
+
+/*
+ * Use this class to make tests 
+ * so u don't have to edit or 
+ * export the hole package
+ */
+import com.numvex.Array;
+import com.numvex.typing.Types;
+
+public class Test {
+    public static void main(String[] args) {
+        Array typed_array = new Array<String>(Types.DOUBLE);
+    }
+}
