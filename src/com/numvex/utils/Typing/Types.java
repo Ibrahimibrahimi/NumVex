98499b640662770c9060c66735c97cfe2ea1efe7
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-09 16:46:10 +0000

    update structure

---
+++ src/com/numvex/utils/Typing/Types.java
@@ -0,0 +1,65 @@
+package com.numvex.typing;
+
+public final class Types {
+    // define types strings
+    public static final String DOUBLE = "double",
+            STRING = "string",
+            INT = "integer",
+            BOOLEAN = "boolean",
+            FLOAT = "float",
+            LONG = "long";
+    public static final String[] SUPPORTED_TYPES = {
+            Types.DOUBLE,
+            Types.INT,
+            Types.STRING,
+            Types.BOOLEAN
+    };
+
+    public static boolean isAllowed(String Type) {
+        for (String type : Types.SUPPORTED_TYPES) {
+            if (type.contains(Type) || type.equals(Type)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String getSupportedTypes() {
+        // convert the list of SUPPORTED_TYPES to string
+        String output = "";
+        String sep = ",";
+        for (String type : Types.SUPPORTED_TYPES) {
+            output += type;
+            output += sep;
+        }
+        return output;
+    }
+
+    public static void TypeError(String type) {
+        throw new IllegalArgumentException("Unsupported type: \"" + type + "\". " +
+                "Supported types:" + Types.getSupportedTypes() + "");
+    }
+
+    public static void ElementTypeError(String type) {
+        // complete this
+    }
+
+    public static boolean CheckType(Object element, String type) {
+        switch (type) {
+            case Types.INT:
+                return element instanceof Integer;
+            case Types.DOUBLE:
+                return element instanceof Double;
+            case Types.FLOAT:
+                return element instanceof Float;
+            case Types.LONG:
+                return element instanceof Long;
+            case Types.STRING:
+                return element instanceof String;
+            case Types.BOOLEAN:
+                return element instanceof Boolean;
+            default:
+                return false;
+        }
+    }
+}
